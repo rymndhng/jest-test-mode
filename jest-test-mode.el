d9b1f78dd97eee04202c941c532055f226c2c36f
Raymond Huang <rymndhng@gmail.com>
2020-03-13 22:32:45 -0700

    Fix checkdoc errors

--- jest-test-mode.el
+++ jest-test-mode.el
@@ -1,4 +1,4 @@
-;;; jest-test-mode.el --- Minor mode for running Node.js tests using jest
+;;; jest-test-mode.el --- Minor mode for running Node.js tests using jest -*- lexical-binding: t; -*-
 
 ;; Author: Raymond Huang <rymndhng@gmail.com>
 ;; URL: https://github.com/rymndhng/jest-test-mode.el
@@ -24,20 +24,22 @@
 ;; C-C C-t t    - Runs describe block at point
 ;; C-C C-t a    - Re-runs the last test command
 
+;;; Code:
+
 (defgroup jest-test nil
   "Minor mode providing commands for running jest tests in Node.js"
   :group 'js)
 
 (defcustom jest-test-options
   '("--color")
-  "Pass extra comand line options to jest when running tests"
+  "Pass extra comand line options to jest when running tests."
   :initialize 'custom-initialize-default
   :type '(list)
   :group 'jest-test-mode)
 
 (defcustom jest-test-npx-options
   '()
-  "Pass extra command line arguments to npx when running tests"
+  "Pass extra command line arguments to npx when running tests."
   :initialize 'custom-initialize-default
   :type '(list)
   :group 'jest-test-mode)
@@ -67,19 +69,21 @@ mode"
   :group 'jest-test-mode)
 
 (defmacro jest-test-from-project-directory (filename form)
-  "Set to npm project root inferred from FILENAME and run the provided FROM with `default-directory` bound.g"
+  "Set to npm project root inferred from FILENAME and run the provided FORM with `default-directory` bound."
   (declare (indent 1))
   `(let ((default-directory (or (jest-test-project-root ,filename)
                                 default-directory)))
      ,form))
 
 (defmacro jest-test-with-debug-flags (form)
+  "Execute FORM with debugger flags set."
   (declare (indent 0))
   `(let ((jest-test-options (seq-concatenate 'list jest-test-options (list "--runInBand") ))
          (jest-test-npx-options (seq-concatenate 'list jest-test-npx-options (list "--node-arg" "inspect"))))
      ,form))
 
 (defun jest-test-project-root (filename)
+  "Find project folder containing a package.json containing FILENAME."
   (if (jest-test-npm-project-root-p filename)
       filename
     (and filename
@@ -89,17 +93,18 @@ mode"
            (directory-file-name (file-name-directory filename)))))))
 
 (defun jest-test-npm-project-root-p (directory)
+  "Check if DIRECTORY contain a package.json file."
   (file-exists-p (concat (file-name-as-directory directory) "/package.json")))
 
 (defun jest-test-find-file ()
-  "Finds the testfile to run. Assumed to be the current file."
+  "Find the testfile to run. Assumed to be the current file."
   (buffer-file-name))
 
 (defvar jest-test-not-found-message "No test among visible bufers")
 
 ;;;###autoload
 (defun jest-test-run ()
-  "Runs the current buffer's file as a test"
+  "Run the current buffer's file as a test."
   (interactive)
   (let ((filename (jest-test-find-file)))
     (if filename
@@ -108,19 +113,19 @@ mode"
       (message jest-test-not-found-message))))
 
 (defun jest-test-run-all-tests ()
-  "Runs all tests in the project"
+  "Run all test in the project."
   (interactive)
   (jest-test-from-project-directory (buffer-file-name)
     (jest-test-run-command (jest-test-command ""))))
 
 (defun jest-test-rerun-test ()
-  "Runs the previously run test in the project"
+  "Run the previously run test in the project."
   (interactive)
   (jest-test-from-project-directory (buffer-file-name)
     (jest-test-run-command jest-test-last-test-command)))
 
 (defun jest-test-run-at-point ()
-  "Runs the top level describe block of the current buffer's point"
+  "Run the top level describe block of the current buffer's point."
   (interactive)
   (let ((filename (jest-test-find-file))
         (example  (jest-test-example-at-point)))
@@ -131,26 +136,26 @@ mode"
       (message jest-test-not-found-message))))
 
 (defun jest-test-debug ()
-  "Runs the test with an inline debugger attached"
+  "Run the test with an inline debugger attached."
   (interactive)
   (jest-test-with-debug-flags
     (jest-test-run)))
 
 (defun jest-test-debug-rerun-test ()
-  "Runs the tests with an inline debugger attached"
+  "Run the test with an inline debugger attached."
   (interactive)
   (jest-test-with-debug-flags
     (jest-test-rerun-test)))
 
 (defun jest-test-debug-run-at-point ()
-  "Runs the tests with an inline debugger attached"
+  "Run the test with an inline debugger attached."
   (interactive)
   (jest-test-with-debug-flags
     (jest-test-run-at-point)))
 
 
 (defun jest-test-example-at-point ()
-  "Finds the topmost describe block from where the cursor is and extract the name"
+  "Find the topmost describe block from where the cursor is and extract the name."
   (save-excursion
     (re-search-backward "^describe")
     (let ((text (thing-at-point 'line t)))
@@ -162,11 +167,11 @@ mode"
   "The last test command ran with.")
 
 (defun jest-test-update-last-test (command)
-  "Updates the last test command"
+  "Update the last test COMMAND."
   (setq jest-test-last-test-command command))
 
 (defun jest-test-run-command (command)
-  "Runs compilation COMMAND in NPM project root."
+  "Run compilation COMMAND in NPM project root."
   (jest-test-update-last-test command)
   (let ((comint-scroll-to-bottom-on-input t)
         (comint-scroll-to-bottom-on-output t)
@@ -177,6 +182,7 @@ mode"
 
 ;;;###autoload
 (defun jest-test-command (filename)
+  "Format test arguments for FILENAME."
   (format "npx %s jest %s %s"
           (mapconcat 'shell-quote-argument jest-test-npx-options " ")
           (mapconcat 'shell-quote-argument jest-test-options " ")
