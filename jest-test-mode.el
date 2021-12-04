e80fab910a6fcef2aa9bee1f582b8e1125675678
Park Hyeonjun <phjosh06@gmail.com>
2021-12-04 15:59:30 +0900

    I made a function to execute a unit test

--- jest-test-mode.el
+++ jest-test-mode.el
@@ -85,6 +85,7 @@
     (define-key map (kbd "C-c C-t n")   'jest-test-run)
     (define-key map (kbd "C-c C-t a")   'jest-test-rerun-test)
     (define-key map (kbd "C-c C-t t")   'jest-test-run-at-point)
+    (define-key map (kbd "C-c C-t u")   'jest-test-run-at-unit)
     (define-key map (kbd "C-c C-t d n") 'jest-test-debug)
     (define-key map (kbd "C-c C-t d a") 'jest-test-debug-rerun-test)
     (define-key map (kbd "C-c C-t d t") 'jest-test-debug-run-at-point)
@@ -171,6 +172,17 @@ mode"
             (jest-test-run-command (jest-test-command filename))))
       (message jest-test-not-found-message))))
 
+(defun jest-test-run-unit ()
+  "Run thes closest test or it block."
+  (interactive)
+  (let ((filename (jest-test-find-file))
+        (example (concat  (jest-test-example-at-point) " " (jest-test-unit-at-point))))
+    (if (and filename example)
+        (jest-test-from-project-directory filename
+          (let ((jest-test-options (seq-concatenate 'list jest-test-options (list "-t" example))))
+            (jest-test-run-command (jest-test-command filename))))
+      (message jest-test-not-found-message))))
+
 (defun jest-test-debug ()
   "Run the test with an inline debugger attached."
   (interactive)
@@ -196,6 +208,15 @@ mode"
     (let ((text (thing-at-point 'line t)))
       (string-match "describe\(\\(.*\\)," text)
       (when-let ((example (match-string 1 text)))
+        (substring example 1 -1)))))
+
+(defun jest-test-unit-at-point ()
+  "Find the topmost it or test block from where the cursor is and extract the name."
+  (save-excursion
+    (re-search-backward "\\(it\([\'\"]\\|test\([\'\"]\\)")
+    (let ((text (thing-at-point 'line t)))
+      (string-match "\\(it\(\\|test\(\\)\\(.*\\)," text)
+      (when-let ((example (match-string 2 text)))
         (substring example 1 -1)))))
 
 (defun jest-test-update-last-test (command)
