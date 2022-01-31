46977f42ea3dfabfb8b870ae0a8c70df9dc27947
Raymond Huang <rayh@hey.com>
2022-01-30 19:01:43 -0800

    test: Add test fixture for manual testing

---
+++ test-data.ts
@@ -0,0 +1,17 @@
+// This is a manual test fixture for trying out jest-test-unit-at-point
+describe("foo", () => {
+
+  test("bar ^s", () => {
+    test("wahooo!", () => {
+
+    })
+  })
+
+  it('hello world', () => {
+
+  })
+
+  it("hello world", () => {
+
+  })
+})
